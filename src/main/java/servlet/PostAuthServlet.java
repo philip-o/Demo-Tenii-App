cdb8dff006e82e7494fda1d185fded6024ad8eb2
Philip Ogunleye <philip_o@hotmail.co.uk>
2018-10-21 08:22:11 +0100

    Fixed redirect

--- src/main/java/servlet/PostAuthServlet.java
+++ src/main/java/servlet/PostAuthServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "PostAuthServlet", urlPatterns = {"/postAuth"})
+@WebServlet(name = "PostAuthServlet", urlPatterns = {"/postauth"})
 public class PostAuthServlet extends HttpServlet {
 
     @Override
