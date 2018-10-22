9f5a17bc6907e850d8ba21bd66b35df12254df41
Philip Ogunleye <philip_o@hotmail.co.uk>
2018-10-22 22:20:24 +0100

    Fixed html

--- src/main/java/servlet/TellerLoginProcessServlet.java
+++ src/main/java/servlet/TellerLoginProcessServlet.java
@@ -55,7 +55,7 @@ public class TellerLoginProcessServlet extends HttpServlet {
         writer.append("</table>");
         writer.append("<br/>");
         writer.append("<br/>");
-        writer.append("<table border=\"1\"><tr><th>Pot Amount</th>><th>Limit</th></tr>");
+        writer.append("<table border=\"1\"><tr><th>Pot Amount</th><th>Limit</th></tr>");
         writer.append("<tr><td>" + pot.getAmount() + "</td><td>" + pot.getLimit() + "</td></tr>");
         writer.append("</table>");
         writer.append("</center>");
