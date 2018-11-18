9d603905d1c6430e22db68f920edd1a8104a494f
Philip Ogunleye <philip_o@hotmail.co.uk>
2018-11-18 18:13:50 +0000

    Added field

--- src/main/java/dtos/trulayer/TrulayerAccounts.java
+++ src/main/java/dtos/trulayer/TrulayerAccounts.java
@@ -8,11 +8,21 @@ public class TrulayerAccounts {
 
     private List<Account> accounts;
 
+    private String accessToken;
+
     public List<Account> getAccounts() {
         return accounts;
     }
 
     public void setResults(List<Account> results) {
         this.accounts = results;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
     }
 }
